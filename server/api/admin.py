764aa324b6591c7df1778fec41edbf695a4c44f3
Ubuntu <ubuntu@ip-172-31-32-188.ap-south-1.compute.internal>
2024-04-29 05:52:10 +0000

    admin.py modified

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'document', 'phone', 'registrationDate')
+    search_fields = ('name', 'email', 'document', 'phone')
+    list_filter = ('registrationDate',)
+
+admin.site.register(User, UserAdmin)
