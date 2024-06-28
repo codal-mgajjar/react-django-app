9105b3bcfc408f8ca02b3a45d2429ef5a034f66f
codal-mgajjar <mgajjar@codal.com>
2024-06-28 14:18:32 +0530

    deployment changes done

--- client/src/components/ListUsers.jsx
+++ client/src/components/ListUsers.jsx
@@ -9,7 +9,7 @@ const ListUsers = ({ users }) => {
         <tr>
           <th>Name</th>
           <th>Email</th>
-          <th>Documentssss</th>
+          <th>Documentsssssssssss</th>
           <th>Phone</th>
           <th>Registration</th>
           <th></th>
