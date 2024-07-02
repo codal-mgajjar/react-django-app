471a5b768d9dda449dc6da29ebd8415a69829615
codal-mgajjar <mgajjar@codal.com>
2024-07-02 18:39:20 +0530

    docker file modified

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -77,16 +77,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'test',
-# 	'USER' : 'test',
-# 	'PASSWORD' : 'test',
-# 	'HOST' : 'database',
-# 	'PORT' : '5432',
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'test',
+	# 'USER' : 'test',
+	# 'PASSWORD' : 'test',
+	# 'HOST' : 'database',
+	# 'PORT' : '5432',
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
