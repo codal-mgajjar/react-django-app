d76d2bce6005a1e34b50b4b78364d3a7ffb9a5ca
codal-mgajjar <mgajjar@codal.com>
2024-07-04 14:46:34 +0530

    yaml added

--- deployapp.sh
+++ deployapp.sh
@@ -4,5 +4,4 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker system prune -f
-docker-compose pull
-docker-compose up -d --build
+docker compose up -d --build
