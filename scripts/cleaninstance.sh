053f9f6edf94814af359cf34b58b60d0705c60ee
codal-mgajjar <mgajjar@codal.com>
2024-07-05 11:21:56 +0530

    changes

--- scripts/cleaninstance.sh
+++ scripts/cleaninstance.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-sudo rm -rf /home/ubuntu/codepipeline/project/react-django-app/* 
+sudo rm -rf /home/ubuntu/codepipeline/project/react-django-app/* &&
+
+sudo docker stop $(docker ps -a -q) &&
+
+# Remove all containers
+sudo docker rm $(docker ps -a -q) &&
+
+# Remove all images
+sudo docker rmi $(docker images -q)
