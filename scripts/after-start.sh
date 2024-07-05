a6d8e4736e338cf3a91b6271be0098bdc6d5bbb2
codal-mgajjar <mgajjar@codal.com>
2024-07-05 11:27:46 +0530

    changes

--- scripts/after-start.sh
+++ scripts/after-start.sh
@@ -1,3 +1,3 @@
 sudo chmod +x /home/ubuntu/codepipeline/project/react-django-app/scripts/boot.sh &&
 cd /home/ubuntu/codepipeline/project/react-django-app &&
-sudo docker volume prune --force
+sudo docker compose down
--- scripts/cleaninstance.sh
+++ scripts/cleaninstance.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-sudo rm -rf /home/ubuntu/codepipeline/project/react-django-app/* &&
-sudo docker stop $(docker ps -a -q)
+sudo rm -rf /home/ubuntu/codepipeline/project/react-django-app/*
