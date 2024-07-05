861b57afcf583deb0f5daecde7759dfefa83e1f2
codal-mgajjar <mgajjar@codal.com>
2024-07-05 12:29:42 +0530

    changes

--- scripts/after-start.sh
+++ scripts/after-start.sh
@@ -1,4 +1,4 @@
 sudo chmod +x /home/ubuntu/codepipeline/project/react-django-app/scripts/boot.sh &&
 cd /home/ubuntu/codepipeline/project/react-django-app &&
 sudo docker compose down &&
-sudo docker rm backend frontend nginx database 
+sudo docker compose up
